35deac36f207e40dd6e12c6cd8c43ddcdca8ee46
KP115386 <KP115386@POOLW42NLPC2020>
2025-03-17 10:52:54 +0530

    Initial commit with basic structure

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CLICalculatorApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
