ad25c7e2f6309893b62a06938cf759655a8a49a6
KP115386 <KP115386@POOLW42NLPC2020>
2025-03-17 11:26:48 +0530

    Created calculator class as well as factory class for operations

---
+++ Calculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLICalculatorApp
+{
+    public sealed class Calculator
+    {
+        private static readonly Calculator instance = new Calculator();
+        public Calculator() { }
+        public static Calculator Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+        public double EvaluateExpression(string expression)
+        {
+            if (expression.Contains("/0") || expression.Contains("/ 0"))
+            {
+                throw new InvalidOperationException("Division by zero is not allowed");
+            }
+            return Convert.ToDouble(new DataTable().Compute(expression, null));
+        }
+    }
+}
---
+++ OperationsFactory .cs
@@ -0,0 +1,26 @@
+﻿using CLICalculatorApp.Operations;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLICalculatorApp
+{
+    public static class OperationsFactory
+    {
+        public static IOperation GetOperation(string operation)
+        {
+            return operation switch
+            {
+                "+" => new Addition(),
+                "-" => new Subtraction(),
+                "*" => new Multiplication(),
+                "/" => new Division(),
+                _ => throw new InvalidOperationException("Invalid operation")
+            };
+        }
+    }
+}
